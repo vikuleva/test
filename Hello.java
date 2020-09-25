63353ec41745a539f69d738d86c3444034fcb428
vikuleva.yu <vikuleva.yu@mail.ru>
2020-09-25 10:43:45 +0300

    Added 2

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 class Hello{
  public static void main(String args[]){
   System.out.println("A");
-  System.out.println("Hello, World!");
+  System.out.println("Hello, World! 2");
   System.out.println("B");
 }}
