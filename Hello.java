bcc185d71681acd9f81cd319379d7dd34922ab5d
vikuleva.yu <vikuleva.yu@mail.ru>
2020-09-25 11:34:04 +0300

    Merge feature-2

--- Hello.java
+++ Hello.java
@@ -1,10 +1,6 @@
 class Hello{
  public static void main(String args[]){
   System.out.println("A");
-<<<<<<< HEAD
-  System.out.println("Hello, World! 1");
-=======
-  System.out.println("Hello, World! 2");
->>>>>>> origin/feature-2
+  System.out.println("Hello, World! 12");
   System.out.println("B");
 }}
